85a3cc6aa37f665c3f1f3ffcc4b4649e6b157c22
acer722 <ahmadzaid2022356@smkn2kra.sch.id>
2023-09-29 10:41:55 +0700

    Menambahkan sistem login

---
+++ application/controllers/Auth.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+	public function index(){
+		$this->load->view('login', array('title' => 'Login'));
+	}
+    public function login(){
+        $this->load->model('User_model');
+        $user = $this->input->post('username');
+        $pass = $this->input->post('password');
+		$cek = $this->User_model->getwu_user($user);
+        if($cek){
+            if(md5($pass) == $cek['password']){
+                $this->session->set_userdata('level', $cek['level']);
+                $this->session->set_userdata('id', $cek['id_user']);
+                $this->session->set_userdata('nama', $cek['nama']);
+                $this->session->set_userdata('username', $cek['username']);
+                redirect(base_url('home'));
+            }else{
+                $this->session->set_flashdata('alert', '
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <i class="fa fa-exclamation-circle me-2"></i> Password Salah!
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                ');
+                redirect(base_url('auth'));
+            }
+        }else{
+            $this->session->set_flashdata('alert', '
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <i class="fa fa-exclamation-circle me-2"></i> Username tidak ada!
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            ');
+            redirect(base_url('auth'));
+        }
+    }
+}
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,14 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model {
-	public function simpan()
-	{
+    protected $_table = 'user'; //nama tabel user
+	public function simpan(){
         $data = array(
             'nama'       => $this->input->post('nama'),
             'username'   => $this->input->post('username'),
             'password'   => md5($this->input->post('password')),
             'level'      => $this->input->post('level'),
         );
-        $this->db->insert('user',$data);
+        $this->db->insert($this->_table,$data);
 	}
+    public function getwu_user($user){
+        return $this->db->get_where($this->_table, array('username' => $user))->row_array();
+    }
 }
